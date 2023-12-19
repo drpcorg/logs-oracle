d616a0861dfa46177610d003d8289cf67488ec33
Nikolay Govorov <me@govorov.online>
2023-12-19 02:06:26 +0000

    Add more methods in java example

--- java/Example.java
+++ java/Example.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.Collections;
 import org.drpc.logsoracle.*;
 
 class Example {
@@ -9,7 +10,9 @@ class Example {
         }
 
         try (LogsOracle db = new LogsOracle(dir.getCanonicalPath(), 0L)) {
+            System.out.println(db.GetLogsCount());
             System.out.println(db.GetBlocksCount());
+            System.out.println(db.Query(0L, 16L, Collections.emptyList(), Collections.emptyList()));
         }
     }
 }
